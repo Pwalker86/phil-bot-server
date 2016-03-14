76ea6ca1dc3d953b7b99660a7d338def466fbc33
Phil Walker <phil1586@gmail.com>
2016-03-14 14:12:36 -0600

    initial commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+require "sinatra"
+require 'blanket'
+
+get "/" do
+  "Hello World!"
+end
+
+get "/insults/get" do
+  "#{Blanket.wrap("http://quandyfactory.com/insult/json").get.insult}"
+end
+
+get "/ermagherd/:string" do
+  response = Blanket.wrap("http://ermahgerd.herokuapp.com/ternslert?value1=#{params[:string]}").get.value1
+  "#{response.gsub(/\//, "")}"
+end
